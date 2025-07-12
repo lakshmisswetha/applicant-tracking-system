925973362d9c2f4b3d9eae0afcafa77cf5d55bbc
lakshmi <lakshmisswetha@gmail.com>
2025-07-12 18:29:51 +0530

    url modified

--- backend/api/app.ts
+++ backend/api/app.ts
@@ -4,7 +4,12 @@ import appRoutes from "./routes/routes";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://applicant-tracking-system.pages.dev/",
+        credentials: true,
+    })
+);
 app.use("/api", appRoutes);
 
 export default app;
